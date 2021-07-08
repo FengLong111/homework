5438128290138ee3f2f499876309c53cd6e4ae47
林品伸 <q22625931@gmail.com>
2021-07-08 13:56:18 +0800

    homework2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please enter number of tickets:");
-        int tick= scanner.nextInt();
-        System.out.print("How many round-trip tickets:");
-        int roundtrip= scanner.nextInt();
-        Tickets tickets = new Tickets(tick, roundtrip);
-        tickets.print();
+        int ticks = 0;
+        while (ticks !=-1){
+            System.out.print("Please enter number of tickets:");
+            ticks= scanner.nextInt();
+            System.out.print("How many round-trip tickets:");
+            int roundtrip= scanner.nextInt();
+            Tickets tickets = new Tickets(ticks, roundtrip);
+            tickets.print();
+            System.out.print("If don,t buy anymore please Enter the -1");
+            ticks= scanner.nextInt();
+            }
+        }
+
+
+
+
     }
-}
+
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,23 +1,37 @@
 package com.train
 
+import sun.security.krb5.internal.Ticket
 import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets:" )
-    var tickets = scanner.nextInt()
-    print("How many round-trip tickets:")
-    var roundtrip = scanner.nextInt()
-    val ticket = Train(tickets,roundtrip)
-    ticket.print()
-
-}
-class Train(var tickets:Int,var roundtrip:Int){
-
-    fun print(){
-        val total = (tickets-roundtrip)*1000+(roundtrip*2000)*0.9
-                println("Total tickets :"+tickets+"\n"
-                +"Round-trip:"+roundtrip+"\n"+"Total:"+total);
+    var tickets = 0
+    while (tickets != -1) {
+        print("Please enter number of tickets:")
+        tickets = scanner.nextInt()
+        if (tickets != -1) {
+            print("How many round-trip tickets:")
+            var roundtrip = scanner.nextInt()
+            val ticke = Train(tickets, roundtrip)
+            ticke.print()
+            print("If don,t buy anymore please Enter the -1"+"\t")
+        }
     }
-
 }
+        class Train(var tickets: Int, var roundtrip: Int) {
+
+            fun print() {
+                val total = (tickets - roundtrip) * 1000 + (roundtrip * 2000) * 0.9
+                println(
+                    "Total tickets :" + tickets + "\n"
+                            + "Round-trip:" + roundtrip + "\n" + "Total:" + total
+                );
+            }
+
+        }
+
+
+
+
+
+
